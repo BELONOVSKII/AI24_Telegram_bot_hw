c37bac85f9a70e0283727448f935083a8596b9ca
ptrbelonovsky <p.belonovsky@gmail.com>
2025-01-27 17:41:52 +0300

    added logging

--- middlewares.py
+++ middlewares.py
@@ -1,8 +1,11 @@
+import logging
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
+log = logging.getLogger(__name__)
 
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
+        log.info(f"Получено сообщение: {event.text}")
         print(f"Получено сообщение: {event.text}")
         return await handler(event, data)
